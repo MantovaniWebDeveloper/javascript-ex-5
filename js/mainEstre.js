651e5316883c7192adfb8e24f32ae684d27d437a
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-22 15:29:55 +0100

    inserimento oggetto nuovo in un array di oggetti, STATICO ok

--- js/mainEstre.js
+++ js/mainEstre.js
@@ -1,3 +1,38 @@
-//creao array
+//creo array
+var studenti = [
+  {
+    "nome" : "Dario",
+    "cognome" : "Mantovani",
+    "eta" : 33
+  },
+  {
+    "nome" : "Andrea",
+    "cognome" : "Saltari",
+    "eta" : 30
+  },
+  {
+    "nome" : "Luca",
+    "cognome" : "Caselli",
+    "eta" : 40
+  }
+];
 
+var nuovoStudente = {
+  "nome" : "Fernando",
+  "cognome" : "Alonso",
+  "eta" : 37
+}
+for (var i = 0; i < studenti.length; i++) {
+  console.log("senza aggiunta: " + studenti[i].nome);
+  console.log("senza aggiunta: " + studenti[i].cognome);
+  console.log("senza aggiunta: " + studenti[i].eta);
+  console.log("")
+}
 //popolo di oggetti
+studenti.push(nuovoStudente);
+for (var i = 0; i < studenti.length; i++) {
+  console.log("con aggiunta: " + studenti[i].nome);
+  console.log("con aggiunta: " + studenti[i].cognome);
+  console.log("con aggiunta: " + studenti[i].eta);
+  console.log("")
+}
