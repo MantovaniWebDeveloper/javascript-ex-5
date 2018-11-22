af3bd53565aa06cb9e1a4dafb77737a57dee974e
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-22 14:01:41 +0100

    creato oggetto studente e stampato per adesso in console le propietà

--- js/main.js
+++ js/main.js
@@ -1,5 +1,13 @@
 //creo l'oggetto studente
-
-//lo popolo di propietà per descrivere uno studente
-
+var studente = {
+  //lo popolo di propietà per descrivere uno studente
+  "nome" : "Dario",
+  "cognome" : "Mantovani",
+  "età" : 33,
+  "corso" : "Boolean",
+  "classe" : 3
+};
 //stampo tutte i valori di tutte le propietà
+for (var k in studente) {
+  console.log(k +" : " + " " + studente[k]);
+}
