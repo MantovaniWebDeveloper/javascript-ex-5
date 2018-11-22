e7058e5fb6310c52d0f3bd793304f9c09f3c433f
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-22 15:39:36 +0100

    Iserimento nuovo oggetto da prompt funzionante

--- js/mainEstre.js
+++ js/mainEstre.js
@@ -16,20 +16,38 @@ var studenti = [
     "eta" : 40
   }
 ];
-
+/* MODALITA' AGGIUNTA STATICA
 var nuovoStudente = {
   "nome" : "Fernando",
   "cognome" : "Alonso",
   "eta" : 37
+}*/
+/*** MODALITA' AGGIUNTA DINAMICA***/
+/*CREO OGGETO VUOTO*/
+var nuovoStudente = {
+
 }
+//RIEMPIO L'OGGETTO DA PROMPT
+var nomeInserito = prompt("Inserisci il tuo nome");
+nuovoStudente.nome = nomeInserito;
+console.log("nome inserito " + nuovoStudente.nome);
+var cognomeInserito = prompt("Inserisci il tuo cognome");
+nuovoStudente.cognome = cognomeInserito;
+console.log("cognome inserito " + nuovoStudente.cognome);
+var etaInserita = parseInt(prompt("Inserisci la tua eta"));
+nuovoStudente.eta = etaInserita;
+console.log("eta inserita " + nuovoStudente.eta);
+
+//ARRAY PRIMA DELLA MODIFICA
 for (var i = 0; i < studenti.length; i++) {
   console.log("senza aggiunta: " + studenti[i].nome);
   console.log("senza aggiunta: " + studenti[i].cognome);
   console.log("senza aggiunta: " + studenti[i].eta);
   console.log("")
 }
-//popolo di oggetti
+//popolo di oggetti nuovi
 studenti.push(nuovoStudente);
+//ARRAY DOPO MODIFICA
 for (var i = 0; i < studenti.length; i++) {
   console.log("con aggiunta: " + studenti[i].nome);
   console.log("con aggiunta: " + studenti[i].cognome);
