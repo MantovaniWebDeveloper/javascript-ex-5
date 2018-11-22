dd37846f8e8a06e3a96f0eab49706e4cc83f4845
MantovaniWebDeveloper <ratedsuperstar84@gmail.com>
2018-11-22 14:51:50 +0100

    creato array studente, inserite porpieta e ciclato in console

--- js/mainEsDue.js
+++ js/mainEsDue.js
@@ -1,8 +1,23 @@
 //creo array
-
-//lo popolo di oggetti Studente
-
-//popolo di propieta ogni oggetto studente
-
-
+var studenti = [
+  //lo popolo di oggetti Studente
+  {
+    //popolo di propieta ogni oggetto studente
+    "nome" : "Dario",
+    "cognome" : "Mantovani"
+  },
+  {
+    "nome" : "Luca",
+    "cognome" : "Caselli"
+  },
+  {
+    "nome" : "Andrea",
+    "cognome" : "Saltari"
+  },
+];
 //navigo e stampo nome e cognome
+var studente;
+for (var i = 0; i < studenti.length; i++) {
+  studente = studenti[i];
+  console.log("Nome e Cognome studente: " + studente.nome + " " + studente.cognome);
+}
